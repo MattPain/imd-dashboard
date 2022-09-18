5c703e3a6d7b816bf04fd31fa459b80a641d7990
Matt Pain <mattpain_569@hotmail.com>
2022-09-18 17:19:41 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+# <- Imports ->
+# High Level Imports
+# import os
+
+# Library Imports
+# import pandas
+# from dash import Dash, dcc, html, Input, Output, State
+# import dash_bootstrap_components as dbc
+# import plotly.express as px
+
+# Module Imports
+# from classes import *moduleName
+# from components import *moduleName
+
+# Data Imports
+# df = pd.read_csv()
+
+# <- Initialise Dash ->
+# For CSS
+# app = Dash(__name__)
+# For Bootstrap - https://dash-bootstrap-components.opensource.faculty.ai/
+# app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+# server = app.server()
+
+# <- App Layout ->
+# app.layout = html.Div()
+
+# <- Callbacks ->
+# @app.callback()
+# def some_func(callback_value)
+
+# <- Run ->
+# if __name__ == '__main__':
+#   app.run_server(debug=True)
